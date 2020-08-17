a9494e78c4ac614350fbd82c641d5ec3eb19e536
Ki Jung Kim <kimkijung@icloud.com>
2020-08-16 21:07:12 -1000

    created constants for messages

---
+++ utilities/constants/message-options/index.js
@@ -0,0 +1,16 @@
+const messageOptions = {
+  attachments: {
+    button: 'button',
+    generic: 'generic'
+  },
+  buttons: {
+    postback: 'postback',
+    webUrl: 'web_url'
+  },
+  quickReplies: {
+    back: 'Back',
+    home: 'Home'
+  }
+}
+
+module.exports = messageOptions;
