1206176b0eb536b76fbac2659686cfa3136c508f
Ki Jung Kim <kimkijung@icloud.com>
2020-08-17 04:08:39 -1000

    added broadcast mockup

--- utilities/reply-handler/index.js
+++ utilities/reply-handler/index.js
@@ -3,7 +3,37 @@ module.exports = (function () {
     axios = require('axios'),
     queries = require('../../db/queries');
 
-  function broadcast() {
+  function broadcast(recipients, message) {
+    const config = {
+      url: 'https://graph.facebook.com/v8.0/me/messages',
+      method: 'post',
+      params: {
+        access_token: process.env.PAGE_ACCESS_TOKEN
+      },
+      data: {
+        message
+      }
+    };
+
+    let counter = 0;
+
+    recipients.forEach((recipient) => {
+      config.data.recipient = {
+        id: recipient.page_user_id
+      }
+
+      // need to throttle below API call
+      return axios(config)
+        .then(() => {
+          console.log(`Broadcast successfully sent to ${++counter} users`);
+
+          return;
+        })
+        .catch((error) => {
+          return queries.errors.insert(error.name, error.message, error.stack);
+        });
+    });
+
     return;
   }
 
@@ -24,11 +54,13 @@ module.exports = (function () {
         data
       };
 
-    axios(config)
+    return axios(config)
       .then((response) => {
         const recipientId = response.data.recipient_id;
 
         console.log(`Message successfully sent to recipient: ${recipientId}`);
+
+        return;
       })
       .catch((error) => {
         return queries.errors.insert(error.name, error.message, error.stack);
