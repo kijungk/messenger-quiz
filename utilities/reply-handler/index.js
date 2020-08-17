dec498a8e1461c59f0c67e44aa97e83d9aa6da30
Ki Jung Kim <kimkijung@icloud.com>
2020-08-16 21:07:44 -1000

    created reply helper

---
+++ utilities/reply-handler/index.js
@@ -0,0 +1,40 @@
+module.exports = (function () {
+  const
+    axios = require('axios'),
+    queries = require('../../db/queries');
+
+  function broadcast() {
+    return;
+  }
+
+  function reply(access_token, recipientId, message) {
+    const
+      data = {
+        recipient: {
+          id: recipientId
+        },
+        message
+      },
+      config = {
+        url: 'https://graph.facebook.com/v8.0/me/messages',
+        method: 'post',
+        params: {
+          access_token
+        },
+        data
+      };
+
+    axios(config)
+      .then((response) => {
+        console.log(`Message successfully sent to recipient: ${response.recipient_id}`);
+      })
+      .catch((error) => {
+        return queries.errors.insert(error.name, error.message, error.stack);
+      });
+  }
+
+  return {
+    broadcast,
+    reply
+  };
+})();
