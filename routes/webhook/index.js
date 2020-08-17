ef13c6be7b9791f5f72d1c2c8b30454e51922437
Ki Jung Kim <kimkijung@icloud.com>
2020-08-16 21:10:47 -1000

    created message handling for post operations

--- routes/webhook/index.js
+++ routes/webhook/index.js
@@ -1,6 +1,14 @@
 const
   express = require('express'),
   httpStatusCodes = require('../../utilities/constants/http-status-codes'),
+  {
+    getMessageByPayload,
+    getPayloadByEvent
+  } = require('../../utilities/payload-handler'),
+  queries = require('../../db/queries'),
+  {
+    reply
+  } = require('../../utilities/reply-handler'),
   router = express.Router();
 
 router.route('/')
@@ -25,6 +33,52 @@ router.route('/')
 
     // all checks passed, webhook challenge can be sent back for verification
     return response.status(httpStatusCodes.ok).send(challenge);
+  })
+  .post((request, response) => {
+    // save body
+    const body = request.body;
+
+    // create variables
+    let entryId, event, senderId, payload, accessToken, eventId, userId;
+
+    // if the body isn't from a page, return a bad request
+    if (body.object !== 'page') {
+      return response.sendStatus(httpStatusCodes.badRequest);
+    }
+
+    body.entry.forEach((entry) => {
+      accessToken = process.env.PAGE_ACCESS_TOKEN; // get access token
+      entryId = entry.id; // the page entry id
+      event = entry.messaging[0]; // the webhook event
+      payload = getPayloadByEvent(event); // get payload based on event type
+      senderId = event.sender.id; // the page-scoped id of event sender
+    });
+
+    return queries.users.fetchByPageUserId(senderId)
+      .then((result) => {
+        const user = result.rows[0]; // user in database
+
+        // if there is no user, create one
+        if (!user) {
+          return queries.users.insert(senderId);
+        }
+
+        return user;
+      })
+      .then((result) => { // result is user
+        // get the response message for a corresponding payload
+        const message = getMessageByPayload(payload);
+
+        return reply(accessToken, senderId, message);
+      })
+      .catch((error) => {
+        // save error to database
+        return queries.errors.insert(error.name, error.message, error.stack);
+      })
+      .finally(() => {
+        // Returns a '200 OK' response to all requests
+        return response.sendStatus(httpStatusCodes.ok);
+      });
   })
 
 module.exports = router;
